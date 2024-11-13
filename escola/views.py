8ae17baf558b82f2a5b1421bbc075520ed4799e4
kelvyntelles <kelvyn.telles@gmail.com>
2024-11-13 16:01:42 -0300

    conhecendo o django rest

---
+++ escola/views.py
@@ -0,0 +1,11 @@
+from django.http import JsonResponse
+
+def estudantes(request):
+    if request.method == 'GET':
+        estudante = {
+            'id':'1',
+            'nome':'Kelvyn'
+        }
+        return JsonResponse(estudante)
+
+
