c853330d4da7254e660f6baec0eefafb3a5a04f0
kelvyntelles <kelvyn.telles@gmail.com>
2024-11-13 18:12:55 -0300

    trabalhando com viewsets

--- escola/serializers.py
+++ escola/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 from escola.models import Estudante, Curso
 
-class EstudanteSerializers(serializers.ModelSerializer):
+class EstudanteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Estudante
         fields = ['id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular']
 
-class CursoSerializers(serializers.ModelSerializer):
+class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
         fields = '__all__'
