b2274f622c5f17a93617d02c083b9d339aa8db3d
kelvyntelles <kelvyn.telles@gmail.com>
2024-11-14 17:00:20 -0300

    criando novas versoes

--- escola/views.py
+++ escola/views.py
@@ -1,17 +1,22 @@
 from escola.models import Estudante, Curso, Matricula
 from escola.serializers import (
     EstudanteSerializer, CursoSerializer, MatriculaSerializer, 
-    ListaMatriculasCursoSerializer, ListaMatriculasEstudanteSerializer
+    ListaMatriculasCursoSerializer, ListaMatriculasEstudanteSerializer, EstudanteSerializerV2
 )
 from rest_framework import viewsets, generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
 
 class EstudanteViewSet(viewsets.ModelViewSet):
     queryset = Estudante.objects.all()
-    serializer_class = EstudanteSerializer
+    # serializer_class = EstudanteSerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     ordering_fields = ['nome']
     search_fields = ['nome']
+    
+    def get_serializer_class(self):
+        if self.request.version == 'v2':
+            return EstudanteSerializerV2
+        return EstudanteSerializer
 
 class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
